dcd912b76be779d89ce6b599b0a21b3faff3be76
ichanmsngkyy <128625300+ichanmsngkyy@users.noreply.github.com>
2025-11-13 04:55:41 +0800

    Latest commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import { AuthProvider } from "./contexts/AuthContext";
 import RegisterForm from "./components/Register";
 import LoginForm from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoutes";
+import HomePage from "./components/HomePage";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,19 +14,15 @@ function App() {
   return (
     <AuthProvider>
       <Router>
-        <nav>
-          <Link to="/register">Register</Link>
-          <Link to="/login">Login</Link>
-          {/* Add more links/tabs here */}
-        </nav>
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/login" element={<LoginForm />} />
           <Route
             path="/dashboard"
             element={
               <ProtectedRoute>
-                <Dashboard />{" "}
+                <Dashboard />
               </ProtectedRoute>
             }
           />
